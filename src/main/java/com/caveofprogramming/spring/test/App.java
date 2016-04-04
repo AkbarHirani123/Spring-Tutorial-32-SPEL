744c69c199bf0c16c87681bcb915111ae687c2ac
Akbar Hirani <akbar.hirani123@gmail.com>
2016-04-04 14:10:29 -0400

    DataAccessException implemented

--- src/main/java/com/caveofprogramming/spring/test/App.java
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -4,22 +4,30 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class App {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
-		
-		OffersDAO offersDao = (OffersDAO)context.getBean("offersDAO");
-		
-		List<Offer> offers = offersDao.getOffers();
-		
-		for(Offer offer : offers) {
-			System.out.println(offer);
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/caveofprogramming/spring/test/beans/beans.xml");
+
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDAO");
+
+		try {
+			List<Offer> offers = offersDao.getOffers();
+
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+		} catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println(ex.getClass());
+
 		}
 		
-		((ClassPathXmlApplicationContext)context).close();
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
