32f31378f4974c986b9a82b18776ba8f37622453
Akbar Hirani <akbar.hirani123@gmail.com>
2016-04-04 14:00:19 -0400

    Querying the database
    using a method called query from jdbc class which takes sql query and
    RowMapper to return a list of Offers. Details in Book.

--- src/main/java/com/caveofprogramming/spring/test/OffersDAO.java
+++ src/main/java/com/caveofprogramming/spring/test/OffersDAO.java
@@ -1,11 +1,14 @@
 package com.caveofprogramming.spring.test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,7 +22,21 @@ public class OffersDAO {
 	}
 	
 	public List<Offer> getOffers(){
-		return null;
+		
+		return jdbc.query("select * from offer", new RowMapper<Offer>() {
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setText(rs.getString("text"));
+				offer.setEmail(rs.getString("email"));
+				
+				return offer;
+			}
+			
+		});
 	}
 	
 }
